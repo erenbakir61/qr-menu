cfe391f53fc52328293a3bf08298e360a03eb55b
EREN <erenbakir61@outlook.com>
2023-03-23 19:21:23 +0300

    admin panel eklendi

---
+++ admin/components/AdminLoginPanel.vue
@@ -0,0 +1,32 @@
+<template>
+  <main id="admin_panel">
+    <section class="login_panel-wrapper">
+      <div class="row">
+        <div class="container">
+          <div class="login_panel text-center">
+            <h2>Login</h2>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style>
+#admin_panel {
+  background-image: url("@/assets/img/login_banner.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+}
+.login_panel {
+  width: 25%;
+  background-color: rgba(220, 216, 216, 0.85);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
+</style>
+
